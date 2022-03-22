038014095fb1862caff7cbe60deb87daef7c0f4c
apolopena <3060702+apolopena@users.noreply.github.com>
2022-03-22 13:44:02 -0700

    👌 IMPROVE: robust release data retrival

--- tools/install.sh
+++ tools/install.sh
@@ -10,19 +10,25 @@
 # Installs gitpod-laravel-starter.
 # Interactive prompts with automated backup when existing project files will be overwritten.
 
-# Globals
-version=
-tarball_url=
+# BEGIN: Globals
+
+# Latest version number for gitpod.laravel-starter. Set via set_release_data()
+latest_version=
+
+# Latest tarball url for gitpod.laravel-starter. Set via set_release_data()
+latest_tarball_url=
+
+# END: GLobals
 
 url_exists() {
-  [[ -z $1 ]] && return 1
+  [[ -z $1 ]] && echo "Internal error: No url argument" && return 1
   if ! curl --head --silent --fail "$1" &> /dev/null; then return 1; fi
 }
 
 get_deps() {
   local url url_root="https://raw.githubusercontent.com/apolopena/gls-tools/main/tools/lib"
   url="$url_root/third-party/spinner.sh"
-  if url_exists ""; then
+  if url_exists "$url"; then
     # shellcheck source=/dev/null
     if ! source <(curl -fsSL "$url"); then echo "Unable to source $url"; return 1; fi
   else
@@ -31,21 +37,47 @@ get_deps() {
   fi
 }
 
+spaces-to-dashes() {
+  echo "$1" | tr ' ' '-'
+}
+
+dashes-to-spaces() {
+  echo "$1" | tr '-' ' '
+}
+
+# Runs any function in this script with an number of args
+# Uses a spinner to inform the user of the progress and tick while the task is performed
+# First argument to this function must be the spinner(user) message
 spinner_task() {
-  local e_pre msg command ec
+  local e_pre msg ec command
   e_pre="Spinner task failed:"
   [[ -z $1 ]] && echo "$e_pre Missing message argument" && return 1
+  msg="$(spaces-to-dashes "$1")"
   [[ -z $2 ]] && echo "$e_pre Missing command argument" && return 1
-  if ! msg="$1" && command="$2" && shift && shift; then
-    echo "$e_pre Could not process arguments" && return 1
-  fi
-  command="$2 $@"
-  echo "debug command=$command"
-  start_spinner "$2" && eval "$command"
+  # For security only eval functions that exist in this script
+  if ! declare -f "$2" > /dev/null; then echo "$e_pre function does not exist: $2" && return 1; fi
+  command="$2"
+  shift; shift
+  start_spinner "$(dashes-to-spaces "$msg") " && eval "$command $*"
   ec=$?
-  [[ $ec != 0 ]] && echo "ERROR: $msg" && stop_spinner 1 && return 1
-  echo "SUCCESS: $msg"
+  [[ $ec != 0 ]] && stop_spinner 1 && return 1
   stop_spinner 0
+}
+
+set_release_data() {
+  local url msg release_json chunk version_regexp
+  version_regex='([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)?'
+  url="https://api.github.com/repos/apolopena/gitpod-laravel-starter/releases/latest"
+
+  # Download
+  if ! url_exists "$url"; then echo "404 error at url: $url" && return 1; fi
+  release_json="$(curl -fsSL "$url")"
+
+  # Parse and set globals
+  chunk="$(echo "$release_json" | grep 'tarball_url')"
+  latest_tarball_url="$(echo "$chunk" | grep -oE 'https.*"')"
+  latest_tarball_url="${latest_tarball_url::-1}"
+  latest_version="$(echo "$chunk" | grep -oE "$version_regex")"
 }
 
 test() {
@@ -55,8 +87,8 @@ test() {
 init() {
   local msg
   if ! get_deps; then echo "Failed to download dependencies" && return 1; fi
-  msg="Downloading latest release data from github"
-  if ! spinner_task "test" "testing1" "testing2" "testing3"; then return 1; fi
+  msg='Downloading latest release data from github'
+  if ! spinner_task "$msg" "set_release_data"; then return 1; fi
 }
 
 main() {
