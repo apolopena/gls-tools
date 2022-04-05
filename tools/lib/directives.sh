17d0932378b8914efcbbf62d71fc10858cac1e11
apolopena <3060702+apolopena@users.noreply.github.com>
2022-04-05 13:19:21 -0700

    ♻️ REFACTOR: recommend-backup(), add --prompt-diffs support and ___backup()

--- tools/lib/directives.sh
+++ tools/lib/directives.sh
@@ -39,7 +39,7 @@ gp_df_only_cache_buster_changed=no
 # Satisfy shellcheck since this is a variable that this script sets but does not use
 : "$gp_df_only_cache_buster_changed"
 # Satisfy shellcheck happy by predefining the colors we use here. See lib/colors.sh
-c_e=; c_norm=; c_norm_prob=; c_fail=; c_file=;
+c_e=; c_norm=; c_norm_prob=; c_pass=; c_fail=; c_file=;
 c_file_name=; c_url=; c_uri=; c_number=; c_choice=; c_prompt=;
 # END: Globals
 
@@ -48,7 +48,7 @@ c_file_name=; c_url=; c_uri=; c_number=; c_choice=; c_prompt=;
 # Description:
 # Echos an error message ($1)
 _directives_err_msg() {
-  echo -e "lib/directives.sh ${c_fail}ERROR:${c_e}\n\t$1"
+  echo -e "${c_file_name}lib/directives.sh ${c_fail}ERROR:${c_e}\n\t$1"
 }
 
 ### default_manifest ###
@@ -122,6 +122,7 @@ set_directives() {
   local e_short_opt="${c_norm_prob} short options are not allowed:"
   local manifest_url="https://raw.githubusercontent.com/apolopena/gls-tools/main/.latest_gls_manifest"
   local note_prefix="${c_file_name}Notice:${c_e}"
+  local long_opt_regexp='^--[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$'
   local chunk manifest manifest_file ec msg1 msg1 arg supported_opts opts=()
 
   supported_opts=(
@@ -131,10 +132,8 @@ set_directives() {
 
   # Handle any options passed into this function
   for arg in "$@"; do
-    # Error out if it is a short option
     [[ $arg == '-' || $arg =~ ^-[^\--].* ]] && _directives_err_msg "$err_pre$e_short_opt $arg" && return 1
-    # Harvest long options and error out if any of them are not supported
-    if [[ $arg =~ ^--[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$ ]]; then
+    if [[ $arg =~ $long_opt_regexp ]]; then
       if ___is_element "$arg" "${supported_opts[@]}"; then
         opts+=("$arg")
       else
@@ -143,11 +142,13 @@ set_directives() {
     fi
   done
 
-  if [[ -n $1 && ! -d $1 ]]; then
-    _directives_err_msg "$err_pre ${c_norm_prob}bad temp directory argument${c_e}"
+  # Handle the optional $1 arg (alternate $tmp_dir location)
+  if [[ -n $1 && ! $1 =~ $long_opt_regexp  && ! -d $1 ]]; then
+    _directives_err_msg "$err_pre ${c_norm_prob}bad temp directory argument: ${c_uri}$1${c_e}"
     return 1
   fi
 
+  # Set the manifest file path
   if [[ -n $1 && -d $1 ]]; then
     manifest_file="$1/$(basename $manifest_url)"
   else
@@ -164,11 +165,13 @@ set_directives() {
     manifest_file="$tmp_dir/$(basename $manifest_url)"
   fi
 
-  # Download the manifest as a file to the temp working directory
+  # Download manifest file and set manifest data
   if ! curl -sf "$manifest_url" -o "$manifest_file"; then
     echo -e "${c_norm_prob} Could not download the updater manifest from ${c_uri}$manifest_url${c_e}"
+
     # clear manifest_file to keep _parse_manifest_chunk() satisfied
     manifest_file=
+
     manifest=$(default_manifest)
     echo -e "${c_norm_prob}Using the default manifest:\n${c_file}$manifest${c_e}\n"
   else
@@ -176,7 +179,7 @@ set_directives() {
   fi
 
   # Parse chunks and convert them to global directive arrays
-  if ___is_element "${opts[@]}" '--keep'; then
+  if ___is_element "${opts[@]}" --only-keep; then
     if has_directive 'keep' "$manifest_file"; then
       chunk="$(_parse_manifest_chunk 'keep' "$manifest")"
       ec=$? && [[ $ec != 0 ]] && echo "$chunk" && return 1
@@ -185,8 +188,7 @@ set_directives() {
       echo -e "${note_prefix}${c_norm}skipping optional directive: ${c_file}keep${c_e}"
     fi
   fi
-
-  if ___is_element "${opts[@]}" '--recommend-backup'; then
+  if ___is_element "${opts[@]}" --only-recommend-backup; then
     if has_directive 'recommend-backup' "$manifest_file"; then
       chunk="$(_parse_manifest_chunk 'recommend-backup' "$manifest")"
       ec=$? && [[ $ec != 0 ]] && echo "$chunk" && return 1
@@ -435,7 +437,7 @@ ___keep() {
     note1b="${c_uri}$orig_loc\n\t${c_file}as per the directive set in the updater manifest\n\t"
     note1c="This action will result in some of the latest changes being omitted${c_e}"
     if [[ $(basename "$orig_loc") == 'init-project.sh' ]]; then
-      echo -e "${c_norm}Keeping project specific file ${c_uri}$orig_loc${c_e}"
+      echo -e "${c_norm}Keeping project specific file:\n${c_uri}$orig_loc${c_e}"
     else
       echo -e "${note1} ${note1b}${note1c}"
     fi
@@ -452,25 +454,16 @@ ___keep() {
 # Requires Globals:
 # $project_root $backups_dir
 ___recommend_backup() {
-  local name orig_loc target_loc err_pre e1_pre msg b_msg1 b_msg2 b_msg2b b_msg3 msg warn1 warn1b cb yn
-  local has_long_option_exists warn_msg_exists note_prefix question instr_file input decor
-  local prompt_again1 prompt_again1b
+  local name orig_loc target_loc err_pre e1_pre msg cb
+  local has_long_option_exists warn_msg_exists instr_file decor
 
   has_long_option_exists="$(declare -f "has_long_option" > /dev/null)"
   warn_msg_exists="$(declare -f "warn_msg" > /dev/null)"
 
   decor="----------------------------------------------"
-  note_prefix="${c_file_name}Notice:${c_e}"
-  yn="${c_e}${c_prompt}(${c_choice}y${c_e}${c_prompt}/${c_e}${c_choice}n${c_prompt})${c_e}"
   name="${c_file_name}recommend_backup()${c_e}"
   err_pre="${c_norm_prob}Failed to ${c_e}$name"
   e1_pre="${c_norm_prob}Could not find${c_e}"
-  b_msg1="${c_file}There is probably project specific data in"
-  question="${c_prompt}Would you like perform the backup now ${yn}${c_prompt}? ${c_e}"
-  warn1="$note_prefix ${c_norm}Answering no${c_e}"
-  warn1b="${c_norm}will skip the backup which will most likely overwrite project specific data.${c_e}"
-  prompt_again1="${c_norm}Please answer ${c_choice}y${c_e}"
-  prompt_again1b="${c_norm}for yes or ${c_choice}n${c_e}${c_norm} for no.${c_e}"
 
   if [[ -z $project_root ]]; then
     _directives_err_msg "$name${c_norm_prob}: required global variable \$project_root${c_e}"
@@ -530,39 +523,19 @@ ___recommend_backup() {
     [[ -d $orig_loc && -d "${target_dir}${1}" ]] \
     && [[ -z $(diff -qr "$orig_loc" "${target_dir}${1}") ]] && return 0
 
-    # For security proceed only if the target is within the project root
+    # For security proceed with the backup only if the target is within the project root
     if ___is_subpath "$project_root" "$target_loc"; then
-      b_msg2="${c_norm}It is recommended that you backup the directory:\n\t"
-      b_msg2b="${c_e}${c_uri}$orig_loc${c_e}\n${c_norm}to\n\t${c_e}${c_uri}$target_loc${c_e}"
-      b_msg3="${c_norm}and merge the contents manually back into the project after the update has succeeded.${c_e}"
-      msg="$b_msg1 ${c_e}${c_uri}$orig_loc${c_e}\n$b_msg2$b_msg2b\n$b_msg3\n$warn1 $warn1b"
-      
-      echo -e "${c_file}${decor}${decor}${c_e}"
-      echo -e "$msg"
 
-      while true; do
-        read -rp "$( echo -e "$question")" input
-        case $input in
-          [Yy]* ) if cp -R "$orig_loc" "$target_loc"; then 
-                    success_msg "${c_file}Backed up the directory ${c_uri}$orig_loc${c_e}"
-                    break 
-                  else
-                    return 1 
-                  fi;;
+      if ! ___backup "directory" "$orig_loc" "$target_loc"; then return 1; fi
 
-          [Nn]* ) echo -e "${note_prefix} ${c_norm_prob}Backup was skipped${c_e}"
-                  echo -e "${c_file}${decor}${decor}${c_e}"
-                  return 0;;
+      # If there is no target directory then the backup was intentionally skipped
+      [[ ! -d $target_loc ]] && return 0
 
-          * ) echo -e "$prompt_again1 $prompt_again1b";;
-        esac
-      done
-
-      # Append merge instructions file for each backup made
+      # Otherwise append merge instructions for each backup to file to locations_map.txt
       instr_file="$backups_dir/locations_map.txt"
       msg="Merge your backed up project specific data:\n\t$target_loc\ninto\n\t$orig_loc"
       if echo -e "${decor}\n$msg\n${decor}\n" >> "$instr_file"; then
-        echo -e "${c_norm}Merge instructions for this file can be found at ${c_uri}$instr_file${c_e}"
+        echo -e "${c_norm}Merge instructions for this file can be found at:\n${c_uri}$instr_file${c_e}"
       else
         echo -e "${c_norm_prob}Error could not create locations map file: ${c_uri}$instr_file${c_e}"
         echo -e "${c_norm_prob}Refer back to this log to manually back up and merge ${c_uri}$target_loc${c_e}"
@@ -616,39 +589,19 @@ ___recommend_backup() {
     fi
   fi
 
-  # For security proceed only if the target is within the project root
+  # For security proceed with the backup only if the target is within the project root
   if ___is_subpath "$project_root" "$target_loc"; then
-    b_msg2="${c_norm}It is recommended that you backup the file:\n\t"
-    b_msg2b="${c_e}${c_uri}$orig_loc\n${c_norm}to\n\t${c_e}${c_uri}$target_loc${c_e}"
-    b_msg3="${c_norm}and merge the contents manually back into the project after the update has succeeded.${c_e}"
-    msg="$b_msg1 ${c_e}${c_uri}$orig_loc\n$b_msg2$b_msg2b\n$b_msg3\n$warn1 $warn1b"
 
-    echo -e "${c_file}${decor}${decor}${c_e}"
-    echo -e "$msg"
+    if ! ___backup "file" "$orig_loc" "$target_loc"; then return 1; fi
 
-    while true; do
-      read -rp "$( echo -e "$question")" input
-      case $input in
-        [Yy]* ) if cp "$orig_loc" "$target_loc"; then
-                  success_msg "${c_file}Backed up the file ${c_uri}$orig_loc${c_e}"
-                  break
-                else
-                  return 1
-                fi;;
+    # If there is no target file then the backup was intentionally skipped
+    [[ ! -f $target_loc ]] && return 0
 
-        [Nn]* ) echo -e "${note_prefix} ${c_norm_prob}Backup was skipped${c_e}"
-                echo -e "${c_file}${decor}${decor}${c_e}"
-                return 0;;
-        
-        * ) echo -e "$prompt_again1 $prompt_again1b";;
-      esac
-    done
-
-    # Append merge instructions for each backup to file
+    # Otherwise append merge instructions for each backup to file to locations_map.txt
     instr_file="$backups_dir/locations_map.txt"
     msg="Merge your backed up project specific data:\n\t$target_loc\ninto\n\t$orig_loc"
     if echo -e "${decor}\n$msg\n${decor}\n" >> "$instr_file"; then
-      echo -e "${c_norm}Merge instructions for this file can be found at ${c_uri}$instr_file${c_e}"
+      echo -e "${c_norm}Merge instructions for the file can be found at:\n${c_uri}$instr_file${c_e}"
     else
       echo -e "${c_norm_prob}Error could not create locations map file ${c_uri}$instr_file${c_e}"
       echo -e "${c_norm_prob}Refer back to this log to manually back up and merge ${c_uri}$target_loc${c_e}"
@@ -658,4 +611,139 @@ ___recommend_backup() {
   fi
   echo -e "$name ${c_norm_prob}failed. Illegal target ${c_uri}$target_loc${c_e}"
   return 1
+}
+
+### ___backup ###
+# Description:
+# Internal function to perform an interactive backup of a file or directory
+# Requires exactly three arguments
+# $1 is the type, either: file or directory
+# $2 is the source location, it must be a valid file or directory
+# $3 is the destination location
+#
+# Note:
+# Behavior of this function depends on if the calling script has certain functions from lib
+# This function will not fail if the calling script is missing certain function from lib
+# If the calling script uses lib\long-option.sh and the global long option --prompt-diffs is passed to
+# the calling script then an additional prompt regarding viewing the differences between the source
+# and the target will occur
+___backup() {
+  local e_pre e_cant_diff decor note_prefix yn question msg b_msg1 b_msg2 b_msg2b b_msg2c warn1 warn1b 
+  local input prompt_again1 prompt_again1b success_msg1 success_msg1b ec
+  e_pre="${c_norm_prob}___prompt_backup() internal error:"
+  e_cant_diff="$e_pre can't diff. bad or missing \$target_dir"
+  [[ $# -ne 3 ]] && _directives_err_msg "$e_pre required exactly 3 arguments, got $#${c_e}" && return 1
+  if [[ -z $1 ]] || [[ $1 != 'file' && $1 != 'directory' ]]; then
+    _directives_err_msg "$e_pre bad type arg: $1. \$1 can only be: file or directory${c_e}" && return 1
+  fi
+
+  # Clear out any possible dynamic scoping so this function does not rely on a callers scope
+  local orig_loc="$2"
+  local target_loc="$3"
+
+  has_long_option_exists="$(declare -f "has_long_option" > /dev/null)"
+  decor="----------------------------------------------"
+  note_prefix="${c_file_name}Notice:${c_e}"
+  yn="${c_e}${c_prompt}(${c_choice}y${c_e}${c_prompt}/${c_e}${c_choice}n${c_prompt})${c_e}"
+  b_msg1="${c_file}There is probably project specific data in"
+  question="${c_prompt}Would you like to perform the backup now ${yn}${c_prompt}? ${c_e}"
+  warn1="$note_prefix ${c_norm}Answering no${c_e}"
+  warn1b="${c_norm}will skip the backup which will most likely overwrite project specific data.${c_e}"
+  prompt_again1="${c_norm}Please answer ${c_choice}y${c_e}"
+  prompt_again1b="${c_norm}for yes or ${c_choice}n${c_e}${c_norm} for no.${c_e}"
+  b_msg2="${c_norm}It is recommended that you back it up to:\n"
+  b_msg2b="${c_uri}${3}${c_e}"
+  b_msg2c="${c_norm}and merge it manually back into the project after the update has succeeded.${c_e}"
+  success_msg1="${c_pass}SUCCESS: ${c_file}Backed up the file:\e[J\n${c_uri}$orig_loc${c_e}"
+  success_msg1b="${c_norm}to${c_e}\n${c_uri}${target_loc}${c_e}"
+  
+  if [[ $1 == 'file' ]]; then
+    [[ ! -f $2 ]] && _directives_err_msg "$e_pre bad source file arg: ${c_uri}${2}${c_e}" && return 1
+    b_msg1="${b_msg1} the file:${c_e}"
+  fi
+
+  if [[ $1 == 'directory' ]]; then
+    b_msg1="${b_msg1} the directory:${c_e}"
+    success_msg1="${success_msg1//file/directory}"
+  fi
+
+  msg="$b_msg1\n${c_uri}$orig_loc\n$b_msg2$b_msg2b\n$b_msg2c"
+  echo -e "${c_file}${decor}${decor}${c_e}"
+  echo -e "$msg"
+  if [[ $has_long_option_exists -eq 0 && $(has_long_option --prompt-diffs; echo $?) == 0 ]]; then
+    [[ ! -d $target_dir ]] && _directives_err_msg "$e_cant_diff" && return 1
+    ___prompt_diff "$orig_loc" "$target_dir/$(basename "$orig_loc")"
+  fi
+  while true; do
+    read -rp "$( echo -e "$question\e[s\n$warn1 $warn1b\e[u\e[1A")" input
+    case $input in
+      [Yy]* ) if [[ $1 == 'file' ]]; then 
+                cp "$orig_loc" "$target_loc"
+              else
+                cp -r "$orig_loc" "$target_loc"
+              fi
+              ec=$?
+              if [[ $ec == 0 ]]; then
+                echo -e "$success_msg1\n$success_msg1b"
+                break
+              else
+                return 1
+              fi;;
+
+      [Nn]* ) echo -e "${note_prefix} ${c_norm_prob}Backup was skipped${c_e}\e[J"
+              echo -e "${c_file}${decor}${decor}${c_e}"
+              return 0;;
+      
+      * ) echo -e "$prompt_again1 $prompt_again1b\e[J";;
+    esac
+  done
+}
+
+### ___show_diff ###
+# Description:
+#
+___show_diff() {
+  local output l1 l2 has_long_option_exists
+  has_long_option_exists="$(declare -f "has_long_option" > /dev/null)"
+  l1="current version"; l2="target version" 
+  [[ -z $target_version ]] && local target_version=
+  if [[ -n $target_version ]]; then
+    l2="${l2} v$target_version"
+  fi
+
+  output="$(diff -ur --minimal --label="$l1" --label="$l2" "$1" "$2")"
+  
+  if declare -f "use_color" > /dev/null; then
+    if declare -f "colorudiff" > /dev/null; then
+      if [[ $(use_color; echo $?) == 0 && $(has_long_option --no-colors; echo $?) -ne 0 ]]; then
+        colorudiff "$output"
+        return
+      fi
+    fi
+  fi
+  echo -e "$output"
+}
+
+### ___prompt_diff ###
+# Description:
+#
+___prompt_diff() {
+  local input question yn prompt_again1 prompt_again1b
+  
+  yn="${c_e}${c_prompt}(${c_choice}y${c_e}${c_prompt}/${c_e}${c_choice}n${c_prompt})${c_e}"
+  question="${c_prompt}Would you like review the differences before deciding to backup ${yn}${c_prompt}? ${c_e}"
+  prompt_again1="${c_norm}Please answer ${c_choice}y${c_e}"
+  prompt_again1b="${c_norm}for yes or ${c_choice}n${c_e}${c_norm} for no.${c_e}"
+
+  while true; do
+    read -rp "$( echo -e "$question")" input
+    case $input in
+      [Yy]* )  ___show_diff "$1" "$2"
+              return 0;;
+
+      [Nn]* ) return 0;;
+
+      * ) echo -e "$prompt_again1 $prompt_again1b";;
+    esac
+  done
 }
